364e6d7682c24796462876afb4d70a4406e4ebbf
Akhad <abdurazakovahad@gmail.com>
2023-03-16 19:33:44 +0300

    Added header_bottom block

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,52 +61,50 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="header__bottom">
+      <div className={styles.header__bottom}>
         <div className="container">
-          <div className="header__bottom-inner">
-            <ul className="menu-categories">
-              <li className="menu-categories__item">
-                <a className="menu-categories__link" href="/">
-                  Квадроциклы
-                </a>
-              </li>
-              <li className="menu-categories__item">
-                <a className="menu-categories__link" href="/">
-                  Катера
-                </a>
-              </li>
-              <li className="menu-categories__item">
-                <a className="menu-categories__link" href="/">
-                  Гидроциклы
-                </a>
-              </li>
-              <li className="menu-categories__item">
-                <a className="menu-categories__link" href="/">
-                  Лодки
-                </a>
-              </li>
-              <li className="menu-categories__item">
-                <a className="menu-categories__link" href="/">
-                  Вездеходы
-                </a>
-              </li>
-              <li className="menu-categories__item">
-                <a className="menu-categories__link" href="/">
-                  Снегоходы
-                </a>
-              </li>
-              <li className="menu-categories__item">
-                <a className="menu-categories__link" href="/">
-                  Двигатели
-                </a>
-              </li>
-              <li className="menu-categories__item">
-                <a className="menu-categories__link" href="/">
-                  Запчасти
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul className={styles.menu_categories}>
+            <li className={styles.menu_categories__item}>
+              <a className={styles.menu_categories__link} href="/">
+                Квадроциклы
+              </a>
+            </li>
+            <li className={styles.menu_categories__item}>
+              <a className={styles.menu_categories__link} href="/">
+                Катера
+              </a>
+            </li>
+            <li className={styles.menu_categories__item}>
+              <a className={styles.menu_categories__link} href="/">
+                Гидроциклы
+              </a>
+            </li>
+            <li className={styles.menu_categories__item}>
+              <a className={styles.menu_categories__link} href="/">
+                Лодки
+              </a>
+            </li>
+            <li className={styles.menu_categories__item}>
+              <a className={styles.menu_categories__link} href="/">
+                Вездеходы
+              </a>
+            </li>
+            <li className={styles.menu_categories__item}>
+              <a className={styles.menu_categories__link} href="/">
+                Снегоходы
+              </a>
+            </li>
+            <li className={styles.menu_categories__item}>
+              <a className={styles.menu_categories__link} href="/">
+                Двигатели
+              </a>
+            </li>
+            <li className={styles.menu_categories__item}>
+              <a className={styles.menu_categories__link} href="/">
+                Запчасти
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
